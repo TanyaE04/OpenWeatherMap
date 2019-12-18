bf5492e750a1cfa0fbb8f876fecee1fde4a7b934
tanyalivanovich <tanya1potapova@gmail.com>
2019-12-18 23:17:21 +0300

    mongoDB

--- src/main/java/by/it/openWeatherMap/MongoDB.java
+++ src/main/java/by/it/openWeatherMap/MongoDB.java
@@ -17,13 +17,13 @@ public class MongoDB {
     public void saveInDB (int status, String date, String body) {
 
         try (MongoClient mongoClient = MongoClients.create(Parameters.MONGODB)) {
-            MongoDatabase database = mongoClient.getDatabase("test");
-            MongoCollection<Document> collection = database.getCollection("openweathermap");
+            MongoDatabase database = mongoClient.getDatabase(Parameters.DATABASE);
+            MongoCollection<Document> collection = database.getCollection(Parameters.COLLECTION);
             Document json = Document.parse(body);
-            Document document = new Document("_id", new ObjectId());
-            document.append("response_date", date)
-                    .append("response", status)
-                    .append("response_body", json);
+            Document document = new Document(Parameters.ID, new ObjectId());
+            document.append(Parameters.DATE_RESPONSE, date)
+                    .append(Parameters.STATUS_RESPONSE, status)
+                    .append(Parameters.BODY_RESPONSE, json);
             collection.insertOne(document);
         }
     }
--- src/main/java/by/it/openWeatherMap/Parameters.java
+++ src/main/java/by/it/openWeatherMap/Parameters.java
@@ -8,4 +8,10 @@ public class Parameters {
     public static final String APPID = bundle.getString("api.key");
     public static final String SCHEDULE = bundle.getString("schedule.cron");
     public static final String MONGODB = bundle.getString("mongodb.uri");
+    public static final String DATABASE = bundle.getString("mongodb.database");
+    public static final String COLLECTION = bundle.getString("mongodb.collection.name");
+    public static final String ID = bundle.getString("mongodb.structure.id");
+    public static final String DATE_RESPONSE = bundle.getString("mongodb.structure.response.date");
+    public static final String STATUS_RESPONSE = bundle.getString("mongodb.structure.response.status");
+    public static final String BODY_RESPONSE = bundle.getString("mongodb.structure.response.body");
 }
