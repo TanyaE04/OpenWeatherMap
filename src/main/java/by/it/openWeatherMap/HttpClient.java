9e6b71eb9af649ddf2eee5061e3db9522eadc972
tanyalivanovich <tanya1potapova@gmail.com>
2019-12-30 17:06:02 +0300

    corrections

--- src/main/java/by/it/openWeatherMap/HttpClient.java
+++ src/main/java/by/it/openWeatherMap/HttpClient.java
@@ -9,19 +9,17 @@ import org.apache.http.impl.client.HttpClients;
 import java.io.IOException;
 
 
-
 public class HttpClient {
 
     public CloseableHttpResponse getResponse() {
 
-        CloseableHttpClient httpClient = HttpClients.createDefault();
-        HttpGet httpGet = new HttpGet(Parameters.URL + Parameters.APPID);
+        HttpGet httpGet = new HttpGet (Parameters.URL + Parameters.APPID);
         CloseableHttpResponse response = null;
-        try {
-            response = httpClient.execute(httpGet);
+        try (CloseableHttpClient httpClient = HttpClients.createDefault ()) {
+            response = httpClient.execute (httpGet);
         } catch (IOException e) {
-            e.printStackTrace();
+            e.printStackTrace ();
         }
-            return response;
-        }
+        return response;
     }
+}
